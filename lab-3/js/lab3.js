f1e1919100d5f77bab97930a5452666b18f3315d
Claire <cameron.liam.yon@gmail.com>
2026-03-20 20:31:34 -0400

    forgot to commit and push to github, but site is functional

--- lab-3/js/lab3.js
+++ lab-3/js/lab3.js
@@ -1,14 +1,37 @@
 const tabs = document.querySelectorAll("[role='tab']");
+const content = document.querySelectorAll("[role='tabpanel']");
 
 tabs.forEach((tab) => {
-    console.log(tab);
+    // console.log(tab);
     tab.addEventListener("click", (event) => {
-		console.log(event);
+		// console.log(event);
 		event.preventDefault();
+
 		tabs.forEach((tab) => {
 			tab.classList.remove("active");
 		});
+		content.forEach((panel) => {
+			panel.classList.remove("show");
+		});
+
 		tab.classList.add("active");
+
+		const id = event.target.getAttribute("href");
+
+		if (id == "#tabpanel1") {
+			content[0].classList.add("show");
+			console.log("Swapped to Panel 1");
+		}
+		else if (id == "#tabpanel2") {
+			content[1].classList.add("show");
+			console.log("Swapped to Panel 2");
+		}
+		else {
+			content[2].classList.add("show");
+			console.log("Swapped to Panel 3");
+		}
 	});
 });
 
+tabs[0].classList.add("active");
+content[0].classList.add("show");
