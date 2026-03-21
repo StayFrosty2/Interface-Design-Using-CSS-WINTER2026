f1d23b74710731d6b7b38dac28e27cfe65c8f878
Claire <cameron.liam.yon@gmail.com>
2026-03-20 20:38:10 -0400

    commented javascript, should be good to submit

--- lab-3/js/lab3.js
+++ lab-3/js/lab3.js
@@ -1,37 +1,40 @@
+// Grab the tabs and panels
 const tabs = document.querySelectorAll("[role='tab']");
-const content = document.querySelectorAll("[role='tabpanel']");
+const panels = document.querySelectorAll("[role='tabpanel']");
 
+// add an event for each tab
 tabs.forEach((tab) => {
-    // console.log(tab);
     tab.addEventListener("click", (event) => {
-		// console.log(event);
+		// prevent the default effect so things don't jump around
 		event.preventDefault();
 
+		// clear the classes from all of the tabs and panels
 		tabs.forEach((tab) => {
 			tab.classList.remove("active");
 		});
-		content.forEach((panel) => {
+		panels.forEach((panel) => {
 			panel.classList.remove("show");
 		});
 
+		// add the "active" class to the current tab
 		tab.classList.add("active");
 
+		// get the href of the current tab
 		const id = event.target.getAttribute("href");
 
+		// short if else string to add the class "show" to the panel, as dictated by the tab
 		if (id == "#tabpanel1") {
-			content[0].classList.add("show");
-			console.log("Swapped to Panel 1");
+			panels[0].classList.add("show");
 		}
 		else if (id == "#tabpanel2") {
-			content[1].classList.add("show");
-			console.log("Swapped to Panel 2");
+			panels[1].classList.add("show");
 		}
 		else {
-			content[2].classList.add("show");
-			console.log("Swapped to Panel 3");
+			panels[2].classList.add("show");
 		}
 	});
 });
 
+// Sets the default tab to be tab 1 and panel 1
 tabs[0].classList.add("active");
-content[0].classList.add("show");
+panels[0].classList.add("show");
